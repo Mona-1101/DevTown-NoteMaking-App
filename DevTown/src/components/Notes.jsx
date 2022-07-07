f6b7fa38917417b1faf53aab94fff3a9b40cd58a
Monalisha <79460728+Mona-1101@users.noreply.github.com>
2022-07-07 21:43:22 +0530

    Add files via upload

---
+++ DevTown/src/components/Notes.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+
+function Notes() {
+  return (
+    <div className="note">
+      <h1>This is the React Bootcamp</h1>
+      <p>
+        Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod
+        tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim
+        veniam.
+      </p>
+    </div>
+  );
+}
+
+export default Notes;
